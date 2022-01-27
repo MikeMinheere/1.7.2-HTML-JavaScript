81c2ed1c08689f49ffc02995d3418442cba1a5f2
Mike Minheere <99067548@mydavinci.nl>
2022-01-27 14:44:28 +0100

    commit

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = 'zet licht aan';
+btn.innerHTML = '...';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+btn.innerHTML = 'turn light on';
 function changeBackground(color) {
     document.body.style.background = color;
  }
- window.addEventListener("load",function() { changeBackground('black') });
- btn.onclick = ("load",function() { changeBackground('yellow') });
- var str = "het licht is aan";
- console.log(str);
+window.addEventListener("load",function() { changeBackground('black') });
+
+A = 1
+var str = "the light is on";
+btn.onclick = (A = 2,"load",function() { changeBackground('yellow') });
+if (A == 2){
+     console.log(str)
+}
+
 // schijf hier tussen je code
